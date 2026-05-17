8719d4e458b474cdaf02df212ab2198997c8805d
brucebruyo <brucebruyo@gmail.com>
2026-05-17 15:57:36 +0100

    modified eslintrc.json

---
+++ .github/workflows/eslint.js
@@ -0,0 +1,24 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+    node: true
+  },
+
+  extends: [
+    'eslint:recommended',
+    'plugin:react/recommended'
+  ],
+
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module'
+  },
+
+  plugins: ['react'],
+
+  rules: {
+    semi: ['error', 'always'],
+    quotes: ['error', 'single']
+  }
+};
